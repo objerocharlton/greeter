c2c9f007bc6fb2945c033e14444bc0b54574dc8c
charlton objero <charlton@charlton-VirtualBox.(none)>
2011-09-07 14:32:05 -0400

    Changed for the second time

--- greeter.rb
+++ greeter.rb
@@ -1,11 +1,41 @@
 #!/usr/bin//env ruby
 # This script will greet people and staff
 
-class Greeter
-	def hello name
-		puts "Hello, #{name}!"	
+Module GreetingAndFarewell
+	def name
+		@name ||= "customer who will give me money for some product/s"
+	end
+
+	def name=  new_name
+		@name = new_name
+	end
+
+	def hello
+		puts "Hello, #{@name}!"	
+	end
+
+	def farewell 
+		puts "Farewell, #{@name}!" 
 	end
 end
-my_greeter = Greeter.new
-my_greeter.hello ARGV.first
+
+class Greeter
+	include GreetingAndFarewell
+	def initialize new_name = nil
+		@name = new_name
+	end
+end
+
+class Customer
+	include GreetingAndFarewell
+	def name
+		@name ||= "new guy will provide me with good or service"
+	end
+end
+
+#my_greeter = Greeter.new
+#my_greeter.hello
+#my_greeter.farewell
+
+
 
