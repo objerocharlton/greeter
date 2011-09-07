c488f3fd4f7a21851da4b2978366a845a0e628a6
charlton objero <charlton@charlton-VirtualBox.(none)>
2011-09-07 13:22:14 -0400

    Changed placeholder file for our greeter class

--- greeter.rb
+++ greeter.rb
@@ -1,4 +1,11 @@
 #!/usr/bin//env ruby
 # This script will greet people and staff
 
+class Greeter
+	def hello name
+		puts "Hello, #{name}!"	
+	end
+end
+my_greeter = Greeter.new
+my_greeter.hello ARGV.first
 
